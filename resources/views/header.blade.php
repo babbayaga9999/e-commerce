8239133546545cb68dc2168b4964d6d9dbd00b93
Ankit Upadhyay <ankitpdh8@gmail.com>
2025-01-30 14:20:46 +0530

    login page done

---
+++ resources/views/header.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">E-Comm</a>
+
+    <!-- Toggler Button -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar Links -->
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home<span class="visually-hidden">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Orders</a>
+        </li>
+
+        <!-- Dropdown -->
+        
+      </ul>
+
+      <!-- Search Form -->
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Submit</button>
+      </form>
+
+      <!-- Right-Aligned Links -->
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Add To Cart</a>
+        </li>
+
+        <!-- Dropdown -->
+        
+      </ul>
+    </div>
+  </div>
+</nav>
